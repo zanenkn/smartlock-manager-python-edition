df9a3f8f515304feda07ad597ab3fd7d0eda53bb
zanenkn <zane@queenslab.se>
2024-11-23 10:32:28 +0100

    just python things

--- controllers/webhooks.py
+++ controllers/webhooks.py
@@ -1,16 +1,19 @@
 from flask import jsonify
 
 
-def handle_create(request):
-    print(f"TRIGGER: a booking with id {request.booking_id} was created")
+def handle_create(req):
+    booking_id = req.get("booking_id")
+    print(f"TRIGGER: a booking with id {booking_id} was created")
     return jsonify({"status": "create processed"}), 200
 
 
-def handle_update(request):
-    print(f"TRIGGER: a booking with id {request.booking_id} was updated")
+def handle_update(req):
+    booking_id = req.get("booking_id")
+    print(f"TRIGGER: a booking with id {booking_id} was updated")
     return jsonify({"status": "update processed"}), 200
 
 
-def handle_cancel(request):
-    print(f"TRIGGER: a booking with id {request.booking_id} was cancelled")
+def handle_cancel(req):
+    booking_id = req.get("booking_id")
+    print(f"TRIGGER: a booking with id {booking_id} was cancelled")
     return jsonify({"status": "cancel processed"}), 200
