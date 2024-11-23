7dbee7712558a45cc3a3797a3cbee9da5c2644e5
zanenkn <zane@queenslab.se>
2024-11-23 17:57:19 +0100

    get_access_code_from_booking_id function

--- controllers/access_codes.py
+++ controllers/access_codes.py
@@ -1,6 +1,7 @@
 import os
 import requests
 import logging
+import re
 
 
 def create_access_code(name, starts_at, ends_at, preferred_code_length):
@@ -35,3 +36,42 @@ def create_access_code(name, starts_at, ends_at, preferred_code_length):
             f"{e.response.json() if e.response and e.response.content else str(e)}"
         )
         raise Exception(error_message) from e
+
+
+def find_code_by_booking_id(code_list, booking_id):
+    for code in code_list:
+        match = re.search(r"Booking nr\.\s*(\d+)\s*for", code["name"])
+        if match and match.group(1) == str(booking_id):
+            return code
+    return None
+
+
+def get_access_code_from_booking_id(booking_id):
+    headers = {
+        "Authorization": f"Bearer {os.getenv('SEAM_API_KEY')}",
+        "Content-Type": "application/json",
+    }
+    try:
+        response = requests.get(
+            "https://connect.getseam.com/access_codes/list",
+            params={"device_id": os.getenv("SEAM_DEVICE_ID")},
+            headers=headers,
+        )
+        response.raise_for_status()
+
+        access_code_list = response.json().get("access_codes", [])
+
+        access_code = find_code_by_booking_id(access_code_list, booking_id)
+
+        logging.info(
+            f"SeamAPI: {f"access code located for booking {booking_id}" if access_code else f"no access code attached to booking ${booking_id}"}"
+        )
+
+        return access_code
+
+    except requests.RequestException as error:
+        error_message = (
+            f"Access code for booking nr. {booking_id} could not be found:"
+            f"{error.response.json() if error.response and error.response.content else str(error)}"
+        )
+        raise Exception(error_message) from error
